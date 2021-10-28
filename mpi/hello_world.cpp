7c9b71b16a4a6d7322e7d2079c4806041fe287d3
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-10-29 00:08:07 +0200

    first experiment with MPI

---
+++ mpi/hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+	int rank, size;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	std::cout << "Hello sono il processo n. " << rank << " su " << size << " processi" << std::endl;
+	MPI_Finalize();
+}
