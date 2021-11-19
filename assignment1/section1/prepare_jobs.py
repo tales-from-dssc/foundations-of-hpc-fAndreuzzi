1036f5a0547ada3cb0465fa53066fcb66d108a95
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-11-19 23:21:41 +0100

    use Template instead of format()

--- assignment1/section1/prepare_jobs.py
+++ assignment1/section1/prepare_jobs.py
@@ -1,3 +1,4 @@
+from string import Template
 import sys
 
 n_max = int(sys.argv[1])
@@ -10,13 +11,13 @@ if async == 1:
 template = """
 #!/bin/bash
 ### Job Name
-#PBS -N ring
+#PBS -N ring$P
 ### Project code
-#PBS -l walltime=00:30:00
+#PBS -l walltime=$walltime
 #PBS -q dssc
 ### Merge output and error files
-#PBS -o /u/dssc/fandreuz/HPC/assignment1/ring{}.txt
-#PBS -l select=1:ncpus={}:mpiprocs={}
+#PBS -o /u/dssc/fandreuz/HPC/assignment1/ring$P.txt
+#PBS -l select=1:ncpus=$P:mpiprocs=$P
 ### Send email on abort, begin and end
 #PBS -m abe
 ### Specify mail recipient
@@ -26,15 +27,18 @@ cd /u/dssc/fandreuz/HPC/assignment1
 
 module load openmpi/4.0.3/gnu/9.3.0
 
-mpic++ {} ring.cpp
+mpic++ $async ring.cpp
 
 ### Run the executable
 for n in {{2..1000}};
-do mpirun -np {} a.out && echo "T# STOP";
+do mpirun -np $P a.out && echo "T# STOP";
 done;
 """
 
 for i in range(2,n_max+1):
+	t = Template(template)
+	text = t.substitute({'P': i, 'walltime': '00:30:00', 'async': async_flag})
+
 	text_file = open("ring{}.pbs".format(i), "w")
-	text_file.write(template.format(i,i,i,async_flag,i))
+	text_file.write(text)
 	text_file.close()
