2952de962ae1d66666277c3672462ba0a45078cd
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-11-21 11:07:44 +0100

    parameter n_min. no more 'T# STOP' (only main process time is evaluated)

--- assignment1/section1/gather_data.py
+++ assignment1/section1/gather_data.py
@@ -2,33 +2,27 @@ import numpy as np
 import sys
 import os
 
-n_max = int(sys.argv[1])
+argc = len(sys.argv)
+if argc == 2:
+	n_max = int(sys.argv[1])
+	n_min = 2
+elif argc == 3:
+	n_max = int(sys.argv[2])
+	n_min = int(sys.argv[1])
 
-data = np.zeros((n_max-1, 3))
+data = np.zeros((n_max-n_min+1, 4))
 
-for i in range(2,n_max+1):
-	stream = os.popen('cat ring{}.txt | grep T#'.format(i))
+for i in range(n_min,n_max+1):
+	stream = os.popen('cat ring{}.txt'.format(i))
 	output = stream.readlines()
         
-	sample_idx = 0
-	samples = np.zeros((1000-1,i))
-
-	values = np.zeros(i)
-	value_idx = 0
+	samples = []
 	for line in output:
-		if 'STOP' in line:
-			samples[sample_idx] = values
-			sample_idx += 1
-			
-			# reset
-			values = np.zeros(i)
-			value_idx = 0
-		else:
-			values[value_idx] = float(line[3:])
-			value_idx += 1
+		samples.append(float(line[3:]))
 
-	data[i-2,0] = np.max(samples)
-	data[i-2,1] = np.min(samples)
-	data[i-2,2] = np.mean(np.mean(samples, axis=1))
+	data[i-n_min,0] = np.max(samples)
+	data[i-n_min,1] = np.min(samples)
+	data[i-n_min,2] = np.mean(samples)
+	data[i-n_min,3] = np.std(samples)
 
 np.save('results.npy', data)
