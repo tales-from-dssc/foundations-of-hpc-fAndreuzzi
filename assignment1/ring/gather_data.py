257d9f52bbd80cea2501d47f2cf082b03c4a2870
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-12-06 14:33:29 +0100

    remove multiple optimizations. comments. code cleanup

--- assignment1/ring/gather_data.py
+++ assignment1/ring/gather_data.py
@@ -10,29 +10,37 @@ elif argc == 3:
 	n_max = int(sys.argv[2])
 	n_min = int(sys.argv[1])
 
-data = np.zeros((n_max-n_min+1, 5, 4))
+# 0->P
+# 1->mapby
+# 2->max,min,mean,std
+data = np.zeros((n_max-n_min+1, 3, 4))
 
 for i in range(n_min,n_max+1):
 	print('Processing {} processors'.format(i))
-	stream = os.popen('cat ring{}.txt'.format(i))
+	stream = os.popen('cat results/ring{}.txt'.format(i))
 	output = list(stream.readlines())
 
-	samples = []
-	idx = 0
-	for opt_idx in range(data.shape[1]):
-		line = output[idx].strip()
-		while line != 'STOP' and idx < len(output):
-			samples.append(float(line))
+	idx = 0        
+	for mb_idx in range(data.shape[1]):
+		samples = []
 
+		if idx < len(output):
+			line = output[idx].strip()
+		else:
+			print('\tnothing here')
+			break
+		while line != 'DONE' and idx < len(output):
+			samples.append(float(line))
+			# get next line
 			idx += 1
 			line = output[idx].strip()
 		# me move on after encountering stop
-		print('\tSTOP found')
+		print('\tDONE found')
 		idx += 1
 
-		data[i-n_min,opt_idx,0] = np.max(samples)
-		data[i-n_min,opt_idx,1] = np.min(samples)
-		data[i-n_min,opt_idx,2] = np.mean(samples)
-		data[i-n_min,opt_idx,3] = np.std(samples)
+		data[i-n_min,mb_idx,0] = np.max(samples)
+		data[i-n_min,mb_idx,1] = np.min(samples)
+		data[i-n_min,mb_idx,2] = np.mean(samples)
+		data[i-n_min,mb_idx,3] = np.std(samples)
 
 np.save('results.npy', data)
