8483f3398ec43fdb712217f165b54f8903cdc6e5
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-12-10 10:56:43 +0100

    code cleanup (to be completed)

--- assignment1/jacobi/scalability.py
+++ assignment1/jacobi/scalability.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 from subprocess import Popen, getstatusoutput, PIPE
+from pln import compute_pln
 
 L = 1200
 
@@ -11,17 +12,38 @@ nprocs = nprocs_map[sys.argv[1]]
 file_names = ['{}{}_{}'.format(sys.argv[1], i, sys.argv[2]) for i in nprocs]
 data = [np.load('results/{}.npy'.format(label)) for label in file_names]
 
-# get latency and bandiwdth
+serial_thin_jacobi = np.mean(np.load('results/serial_thin.npy')[2:-2, 2:4])
+serial_gpu_jacobi = np.mean(np.load('results/serial_gpu.npy')[2:-2, 2:4])
+# you need to set this somewhere
+serial_time = None
+
+serial_thin_lup = np.mean(np.load('results/serial_thin.npy')[2:-2, -1])
+serial_gpu_lup = np.mean(np.load('results/serial_gpu.npy')[2:-2, -1])
+# you need to set this somewhere
+serial_lup = None
 
 if sys.argv[1] == 'serial':
 	raise ValueError('scalability is not defined in this case')
 elif sys.argv[1] == 'onenode':
 	third = sys.argv[2]
 	fourth = 'ngpu'
+
+	serial_time = serial_thin_jacobi
+	serial_lup = serial_thin_lup
 elif sys.argv[1] == 'twonodes':
 	fourth = sys.argv[2]
 	third = 'socket'
 
+	if fourth == 'gpu':
+		serial_time = serial_thin_jacobi
+		serial_lup = serial_thin_lup
+	else:
+		serial_time = serial_gpu_jacobi
+		serial_lup = serial_gpu_lup
+
+print('found serial time {} and LUP {}'.format(serial_time, serial_lup))
+
+# get latency and bandiwdth
 cmd = 'python3 ../benchmark/fit.py nintel infiniband {} {}'.format(third, fourth)
 print(cmd)
 result = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
@@ -30,47 +52,42 @@ output = iter(result.stdout.readline, b'')
 buff = []
 for line in output:
 	buff.append(float(line))
-print('latency, bandwidth ', buff)
 latency = buff[0] * 1.e-6
 bandwidth = buff[1]
+print('lat: {}, band: {}'.format(latency, bandwidth))
 
 #
 
-def estimate_pln(jacobi_time, Tc, N):
-	return L*L*L * N / (jacobi_time + Tc)
+PLN = compute_pln(data, serial_time, latency=latency, bandwidth=bandwidth, L=L)
 
-PLN = []
-for dt in data:
-	k = np.count_nonzero(dt[0,:3] - 1) * 2
-	c = (L*L * k * 2 * 8) * pow(2,-20)
-	print(c / bandwidth, k*latency)
-	Tc = c / bandwidth + k * latency
-	N = np.product(dt[0,:3])
+if len(sys.argv) == 4:
+	if sys.argv[3] == '0':
+		measured_mlup = np.array(data)[:,:,-1]
+		mlup = np.mean(measured_mlup[:,2:-2], axis=1)
 
-	print('k: {}, c: {}, Tc: {}, N: {}'.format(k, c, Tc, N))
-	PLN.append([estimate_pln(dt[1:,2], Tc, N), estimate_pln(dt[1:,3], Tc, N)])
-PLN = np.array(PLN)
+		plt.plot(nprocs, PLN*1.e-6, 'ro-', label='$P(L,N)$')
+		plt.plot(nprocs, mlup, 'bo-', label='measured')
+		plt.plot(nprocs, np.array(nprocs) * serial_lup, label='N$P_1(L)$')
 
-for idx,i in enumerate(nprocs):
-	print('PLN for {} processors'.format(i))
-	print('using min jacobi time:')
-	print(PLN[idx,0])
-	print('using max jacobi time:')
-	print(PLN[idx,1])
+		plt.yscale('log')
 
-if len(sys.argv) == 4 and sys.argv[3] == '1':
-	measured_mlup = np.array(data)[:,:,-1]
-	mlup = np.mean(measured_mlup[:,2:-2], axis=1)
+		plt.xlabel('P')
+		plt.ylabel('MLUP/sec')
 
-	plt.plot(nprocs, np.mean(PLN[:,0],axis=1)*1.e-6, 'ro-', label='min jacobi time')
-	plt.plot(nprocs, np.mean(PLN[:,1],axis=1)*1.e-6, 'go-', label='max jacobi time')
-	plt.plot(nprocs, mlup, 'bo-', label='measured')
+		plt.title('LUP')
 
-	plt.yscale('log')
+		plt.grid()
+		plt.legend()
+		plt.show()
+	elif sys.argv[3] == '1':
+		measured_mlup = np.array(data)[:,:,-1]
+		mlup = np.mean(measured_mlup[:,2:-2], axis=1)
 
-	plt.xlabel('P')
-	plt.ylabel('MLUP/sec')
+		plt.plot(nprocs, np.array(nprocs) * serial_lup / (PLN*1.e-6), 'ro-')
 
-	plt.grid()
-	plt.legend()
-	plt.show()
+		plt.xlabel('P')
+
+		plt.title('scalability')
+
+		plt.grid()
+		plt.show()
