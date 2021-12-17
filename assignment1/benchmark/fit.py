977ef932bd9d2528d9687cc05e7880dc3b3c4209
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-12-17 14:51:58 +0100

    improve image title

--- assignment1/benchmark/fit.py
+++ assignment1/benchmark/fit.py
@@ -32,7 +32,7 @@ fit_band = fit2[1]
 print(fit_lat)
 print(fit_band)
 
-if len(sys.argv) == 6 and sys.argv[5] == '1':
+if len(sys.argv) == 6:
 	l1, = plt.plot(n1[:,0], func(n1[:,0], fit_lat, fit_band), 'ro-')
 
 	plt.ylabel('$\mu$s')
@@ -43,9 +43,25 @@ if len(sys.argv) == 6 and sys.argv[5] == '1':
 	l2, = plt.plot(n1[:,0], n1[:,2], 'go-')
 
 	plt.legend([l1, l2], ['Fit', 'Measured'])
-	plt.title(' '.join(sys.argv[1:5]))
+
+	lb = ' '.join(sys.argv[1:5])
+
+	lb = lb.replace('nintel', 'OpenMPI')
+	lb = lb.replace('intel', 'IntelMPI')
+	lb = lb.replace('infiniband', 'InfiniBand')
+	lb = lb.replace('gigabit', 'Gigabit')
+	lb = lb.replace('ngpu', 'THIN')
+	lb = lb.replace('gpu', 'GPU')
+	lb = lb.replace('core', '--map-by core')
+	lb = lb.replace('socket', '--map-by socket')
+	lb = lb.replace('node', '--map-by node')
+	plt.title(lb)
+
 	plt.grid()
 
 	plt.xticks(n1[:,0][1::3], ['$2^{{{}}}$'.format(int(log2(n))) for n in n1[:,0][1::3]])
 
-	plt.show()
+	if sys.argv[5] == '1':
+		plt.show()
+	else:
+		plt.savefig('imgs/{}_{}_{}_{}.png'.format(*sys.argv[1:5]))
